ef5d25986acdfd469178c54ab4a866483ee37d6c
Mayank Kashyap <mayank@xerocodee.com>
2023-07-13 20:57:02 +0530

    Modifying Submission Code

--- src/question/question.service.ts
+++ src/question/question.service.ts
@@ -2,8 +2,8 @@ import { Model } from "mongoose";
 import Service from "../globalinterfaces/service.interface";
 import { IQuestion, ITC } from "./question.interface";
 import questionModel from "./question.model";
-import { AxiosInstance } from 'axios';
-import * as axios from 'axios';
+import { AxiosInstance } from "axios";
+import axios = require("axios");
 import HTTPException from "../exceptions/http.exception";
 import * as dotenv from "dotenv";
 dotenv.config();
@@ -79,13 +79,14 @@ class QuestionService implements Service {
       throw error;
     }
   }
+
   public submitQuestion = async (questionId: string, submissionData: any) => {
     try {
       const getQuestion = await questionModel.findById(questionId);
       if (!getQuestion) {
         throw new HTTPException(400, "Question not Found");
       }
-  
+
       const testCases: ITC[] = getQuestion.testCases;
       const submissionPromises = testCases.map((tc: ITC) => {
         const payload = {
@@ -94,8 +95,8 @@ class QuestionService implements Service {
           input: tc.input,
           expectedOutput: tc.output,
         };
-  
-        return instance
+
+        return axios
           .post(
             `https://api.compilers.sphere-engine.com/api/v4/submissions?access_token=${process.env.api_token}`,
             payload
@@ -106,92 +107,62 @@ class QuestionService implements Service {
             } else {
               throw new HTTPException(400, "Error");
             }
-          })
-          .catch((error) => {
-            throw new HTTPException(400, "Error");
           });
       });
-  
+
       const submissionResults = await Promise.all(submissionPromises);
-  
+
       if (submissionResults.every((result) => result != null)) {
-        const submissionExistPromises = submissionResults.map((submissionId) => {
-          return new Promise(async (resolve, reject) => {
-            try {
-              const response = await instance.get(
-                `https://api.compilers.sphere-engine.com/api/v4/submissions/${submissionId}?access_token=${process.env.api_token}`
+        const resultPromises = submissionResults.map((submissionId) => {
+          return new Promise((resolve) => {
+            setTimeout(async () => {
+              const response = await axios.get(
+                `https://api.compilers.sphere-engine.com/api/v4/submissions/${submissionId}/output?access_token=${process.env.api_token}`
               );
-  
+
               if (response && response.data) {
                 resolve(response.data);
               } else {
                 throw new HTTPException(400, "Error");
               }
-            } catch (error) {
-              reject(new HTTPException(400, "Error"));
-            }
+            }, 3000); // Delay of 3 seconds
           });
         });
-  
-        const submissionExistResults = await Promise.all(submissionExistPromises);
-  
-        if (submissionExistResults.every((result) => result != null)) {
-          const resultPromises = submissionResults.map((submissionId) => {
-            return new Promise(async (resolve, reject) => {
-              try {
-                const response = await instance.get(
-                  `https://api.compilers.sphere-engine.com/api/v4/submissions/${submissionId}/output?access_token=${process.env.api_token}`
-                );
-  
-                if (response && response.data) {
-                  resolve(response.data);
-                } else {
-                  throw new HTTPException(400, "Error");
-                }
-              } catch (error) {
-                reject(new HTTPException(400, "Error"));
-              }
-            });
+
+        const results = await Promise.all(resultPromises);
+
+        if (results.every((result) => result != null)) {
+          const outputMatcher = results.map((op, index) => {
+            console.log(op, testCases[index].output);
+            return op == testCases[index].output;
           });
-  
-          const results = await Promise.all(resultPromises);
-  
-          if (results.every((result) => result != null)) {
-            const outputMatcher = results.map((op, index) => {
-              console.log(op, testCases[index].output);
-              return op == testCases[index].output;
-            });
-  
-            const totalCorrect = outputMatcher.reduce(
-              (acc, curr) => acc + (curr ? 1 : 0),
-              0
-            );
-  
-            let status = "error";
-            switch (true) {
-              case totalCorrect == outputMatcher.length:
-                status = "success";
-                break;
-              case totalCorrect < outputMatcher.length:
-                status = "wrong";
-                break;
-              default:
-                status = "error";
-            }
-  
-            return {
-              outputArray: outputMatcher,
-              correct: totalCorrect + " out of " + outputMatcher.length,
-              status: status,
-            };
+
+          const totalCorrect = outputMatcher.reduce(
+            (acc, curr) => acc + (curr ? 1 : 0),
+            0
+          );
+
+          let status = "error";
+          switch (true) {
+            case totalCorrect == outputMatcher.length:
+              status = "success";
+              break;
+            case totalCorrect < outputMatcher.length:
+              status = "wrong";
+              break;
+            default:
+              status = "error";
           }
+
+          return {
+            outputArray: outputMatcher,
+            correct: totalCorrect + " out of " + outputMatcher.length,
+            status: status,
+          };
         }
       }
     } catch (error) {
-      return {
-        status: "error",
-        error: error.message,
-      };
+      throw error;
     }
   };
 }
